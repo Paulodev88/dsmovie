2895d733b898d67d10adf975ec91f4b98f0a2be0
Paulodev88 <paulo.ricardodev88@gmail.com>
2022-01-12 22:22:35 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Paulo Ricardo
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+
+
+function Form() {
+  return <h1>Página Form</h1>;
+};
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 Paulo Ricardo
+
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+function Listing (){
+
+    return (
+        <h1>
+            Página de Listagem
+        </h1>
+    )
+}
+
+export default Listing;
