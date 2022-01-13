a5e61ec7651fd15a5376acf038cc029831cd7df0
Paulodev88 <paulo.ricardodev88@gmail.com>
2022-01-12 21:24:22 -0300

    Project Created

---
+++ backend/src/test/java/com/cl4/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cl4.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
